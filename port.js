2918467ac40dcd141cf27eead40448486cf11bc4
Rajarshi Roy <royrajarshi0123@gmail.com>
2024-04-13 00:35:08 +0530

    check 2

--- port.js
+++ port.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Data Science Enthusiast"],
+        strings: ["Data Science Aspirant"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Data Science Enthusiast"],
+        strings: ["Data Science Aspirant"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
